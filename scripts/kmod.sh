b40e75934392bd42c59c578259e9b1b267b08b3d
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2025-01-08 14:58:19 +0100

    Add option to load module from temp and use sudo

--- scripts/kmod.sh
+++ scripts/kmod.sh
@@ -1,36 +1,108 @@
-#!/bin/sh
+#!/bin/bash
 
 # Reload the kernel module provided as an argument.
 # It is assumed that the kernel module resides at KMOD_BASE/$1/$1.ko
 
 KMOD_BASE=/mnt/host0/kmods
+CWD=$(pwd)
 
-MODULE_NAME="$1"  # Accept module name as a command line argument
+function usage() {
+    echo "Usage: $0 [OPTIONS] <MODULE_NAME>"
+    echo ""
+    echo "Options"
+    echo "  -t, --tmp       Copy module to /tmp and load from there."
+    echo "  -s, --sudo      Use sudo for rmmod and insmod."
+    echo "  -h, --help      Display this help message."
+    echo "  -v, --verbose   Show verbose output."
+    echo ""
+}
 
-# Check if the module name is provided
-if [ -z "$MODULE_NAME" ]; then
-    echo "Usage: $0 <module_name>"
+function log() {
+    if [ "$verbose" = true ]; then
+        printf "$(date +"%H:%M:%S") $* \n"
+    fi
+}
+
+function log_err() {
+    printf "$(date +"%H:%M:%S") ERROR: $* \n" >&2
+}
+
+use_tmp=false
+use_sudo=false
+verbose=false
+module_name=
+
+while [ "$#" -gt 0 ]; do
+    case $1 in 
+        -h|--help)
+            usage
+            exit
+            ;;
+        -t|--tmp)
+            use_tmp=true
+            shift
+            ;;
+        -s|--sudo)
+            use_sudo=true
+            shift
+            ;;
+        -v|--verbose)
+            verbose=true
+            shift
+            ;;
+        -*)
+            log_err "Unknown option \"$1\""
+            usage
+            exit 1
+            ;;
+        *)
+            if [ -n "$module_name" ]; then
+                log_err "Only one module can be specified"
+                usage
+                exit 1
+            fi
+            module_name=$1
+            shift
+    esac
+done
+
+if [ -z "$module_name" ]; then
+    log "Required argument MODULE_NAME missing"
+    usage
     exit 1
 fi
 
-module_path="${KMOD_BASE}/${MODULE_NAME}/${MODULE_NAME}.ko"
+module_file=$module_name.ko
+module_path=$(find . -name $module_file -exec realpath {} \;)
 
-if [ ! -f "$module_path" ]; then
-    echo "$module_path does not exist"
-    exit
+if [ -z "$module_path" ]; then
+    echo "No file named $module_file found in subdirectories"
+    exit 1
 fi
 
-if lsmod | grep -q "$MODULE_NAME"; then
-    echo "Module '$MODULE_NAME' is loaded. Reloading..."
-    rmmod "$MODULE_NAME"
-    insmod "$module_path"
-else
-    # echo "Module '$MODULE_NAME' is not loaded. Loading..."
-    insmod "$module_path"
+if [ "$use_tmp" = true ]; then
+    log "Copy module from $module_file to /tmp/$module_file"
+    cp "$module_path" /tmp
+    module_path=/tmp/$module_file
 fi
 
-if lsmod | grep -q "$MODULE_NAME"; then
-    echo "Reloaded module"
+prefix=
+if [ "$use_sudo" = true ]; then
+    log "Use sudo for (re)loading the module"
+    prefix=sudo
+fi
+
+if lsmod | grep -q "$module_name"; then
+    log "Module '$module_name' is loaded. Reloading..."
+    eval "$prefix rmmod $module_name"
+    eval "$prefix insmod $module_path"
 else
-    echo "ERROR: FAILED TO RELOAD MODULE"
+    log "Module '$module_name' is not loaded. Loading..."
+    eval "$prefix insmod $module_path"
+fi
+
+if lsmod | grep -q "$module_name"; then
+    echo "Module $module_name has been (re)loaded"
+else
+    echo "ERROR: FAILED TO (RE)LOAD MODULE $module_name"
 fi
