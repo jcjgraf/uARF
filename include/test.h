222d4562792c91d6f63f7ad9440911560f5b570d
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2025-01-09 16:27:28 +0100

    Make test files more unique by adding prefix

--- include/test.h
+++ include/test.h
@@ -1,4 +1,5 @@
 #include "log.h"
+#include <compiler.h>
 #include <stdlib.h>
 
 #ifdef LOG_TAG
@@ -6,15 +7,18 @@
 #define LOG_TAG LOG_TAG_TEST
 #endif
 
-#define TEST_CASE(name)          static unsigned long __text name()
-#define TEST_CASE_ARG(name, arg) static unsigned long __text name(void *arg)
-#define TEST_SUITE()             int main(void)
+#define TEST_CASE_PREFIX test_case_
+
+#define TEST_CASE(name) static unsigned long __text CAT(TEST_CASE_PREFIX, name)()
+#define TEST_CASE_ARG(name, arg)                                                         \
+    static unsigned long __text CAT(TEST_CASE_PREFIX, name)(void *arg)
+#define TEST_SUITE() int main(void)
 #define RUN_TEST_CASE(name)                                                              \
     LOG_INFO("Run TestCase " STR(name) "\n");                                            \
-    name()
+    CAT(TEST_CASE_PREFIX, name)()
 #define RUN_TEST_CASE_ARG(name, arg)                                                     \
     LOG_INFO("Run TestCase " STR(name) "\n");                                            \
-    name(_ptr(arg))
+    CAT(TEST_CASE_PREFIX, name)(_ptr(arg))
 
 #define TEST_PASS() return 0
 #define TEST_ASSERT(cond)                                                                \
