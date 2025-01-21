a402e847ed9deb99b50256bda55e46c7ce14cbcc
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2025-01-21 12:41:59 +0100

    Rewrite sync to allow for configurable sync jobs

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,120 +1,194 @@
 #!/bin/bash
 
+# Open a new file descriptor that redirects to stdout:
+# For logging
+exec 3>&1
+
 # This script is designed to synchronize all files inside SRC to DST. The files
 # COMMON_EXCLUDE and if -a it not used BUILD_EXCLUDE are excluded. Contains also a
 # daemon that watches the files returned by WATCH_FILES_CMD and triggers a sync upon
 # change.
 
-CWD=$(pwd)
-SCRIPT_PATH=$(realpath "$0")
-SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
-
-SRC="$HOME/CurrentCourse/Code/uARF/" # Trailing slash matters!
-DST="cn128:/data/jegraf/MA/uARF"
-
-COMMON_EXCLUDE=(
-    --exclude=.git \
-    --exclude=.cache \
-)
-
-BUILD_EXCLUDE=(
-    --exclude "*.o" \
-    --exclude "*.bin" \
-    --exclude "*.d" \
-    --exclude "*.a" \
-    --exclude "*.cpio.gz" \
-    --exclude "*.ko" \
-    --exclude "*.cmd" \
-    --exclude "Module.symvers" \
-    --exclude "modules.order" \
-    --exclude "*.mod" \
-    --exclude "*.mod.c" \
-    --exclude=scripts/busybox \
-    --exclude=scripts/initramfs \
-    --exclude=scripts/bzImage \
-)
-
-WATCH_FILES_CMD=(
-    find "$SRC" \
-    -type f \
-    -name "*.c" \
-    -or -name "*.h" \
-    -or -name "*.S" \
-    -or -name ".config" \
-    -or -name "Makefile" \
-    -or -name "*.bin" \
-    -or -name "*.ko" \
-    # -or -name "*.sh" \
-)
-
 # Terminate the synchronization after time time
 # Done because rsync somehow gets blocked sometimes
 SYNC_TIMEOUT=20
 
-all=false
-force=false
-daemon=false
+LONGOPTS=all,daemon,force,verbose,help
+OPTIONS=adfvh
 
 function usage() {
-    echo "Usage: $0 [options]"
+    echo "Usage: $0 [OPTION]... CONFIG"
+    echo ""
     echo "Options:"
-    echo "  -a      Sync all files (including build artefacts etc.)"
-    echo "  -f      Force sync, deleting all files at target that are not in the source."
-    echo "  -d      Start sync daemon"
-    echo "  -h      Show this help message"
-    exit 1
+    echo "  --all, -a      Sync all files (including build artefacts etc.)"
+    echo "  --daemon, -d   Start sync daemon."
+    echo "  --force, -f    Delete all files at destination that do not exist at source."
+    echo "  --verbose, -v  Verbose logging."
+    echo "  --help, -h     Show this help message"
+    echo ""
+    echo "Positional Arguments:"
+    echo "  CONFIG: Path to configuration file to use."
+    echo ""
+    echo "Configuration:"
+    echo "  Store in file and provide path as CONFIG"
+    echo "  All options are required"
+    echo "  All options are required"
+    echo "  Options:"
+    echo "    SRC:              Source path (trailing / matters!)"
+    echo "    DEST:             Destination path"
+    echo "    COMMON_EXCLUDE:   List of files that are always excluded"
+    echo "    BUILD_EXCLUDE:    List of files that are excluded, unless --all is used"
+    echo "    WATCH:            List of files to watch"
 }
 
 function log() {
-    printf "$(date +"%H:%M:%S") $* \n"
+    if [ "$verbose" = true ]; then
+        printf "$(date +"%H:%M:%S") $* \n" 1>&3
+    fi
 }
 
 function log_err() {
     printf "$(date +"%H:%M:%S") ERROR: $* \n" >&2
+}
+
+function log_err_msg() {
+    log_err $*
     notify-send -u Critical "SYNC ERROR" "$*\n"
 }
 
-# Parse options
-while getopts "afdh" opt; do
-    case $opt in
-        a) all=true ;;
-        f) force=true ;;
-        d) daemon=true ;;
-        h) usage ;;
-        *) usage ;;
+# -temporarily store output to be able to check for errors
+# -activate quoting/enhanced mode (e.g. by writing out “--options”)
+# -pass arguments only via   -- "$@"   to separate them correctly
+# -if getopt fails, it complains itself to stdout
+PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@") || exit 2
+# read getopt’s output this way to handle the quoting right:
+eval set -- "$PARSED"
+
+all=false
+daemon=false
+force=false
+verbose=false
+
+# Read till we see --
+while true; do
+    case "$1" in
+        -a|--all)
+            all=true
+            shift
+            ;;
+        -d|--daemon)
+            daemon=true
+            shift
+            ;;
+        -f|--force)
+            force=true
+            shift
+            ;;
+        -v|--verbose)
+            verbose=true
+            shift
+            ;;
+        -h|--help)
+            usage
+            exit 1
+            ;;
+        --)
+            shift
+            break;
+            ;;
+        *)
+            echo "Invalid option \"$1\""
+            usage
+            exit 3
+            ;;
     esac
 done
 
+# handle non-option arguments
+if [[ $# -ne 1 ]]; then
+    echo "CONFIG file missing"
+    usage
+    exit 4
+fi
+
+log "All arguments parsed"
+
+config=$(readlink -f "$1")
+
+if [ ! -f "$config" ]; then
+    echo "Config file \"$config\" does not exist"
+    exit 5
+fi
+
+log "Sourcing config \"$config\""
+. $config
+log "Sourced config"
+
+# Check that there is a config option named $1 and it has a value
+function check_config_option() {
+    log "Checking config option $1"
+    local opt_name=$1
+    local opt=${!opt_name} # Convert from string to variable
+    echo $opt_name
+    echo $opt
+    if [ -v "$opt" ]; then
+        echo "Missing config option \"$opt_name\""
+        exit 5
+    else
+        log "Config option $opt_name exists and has value:\n$opt"
+    fi
+}
+
+check_config_option SRC
+check_config_option DST
+# check_config_option EXCLUDE_BASE
+# check_config_option EXCLUDE_EXTENDED
+check_config_option WATCH
+
 # Composes the rsync command, based on the cli arguments
 function get_sync_cmd() {
+    log "Composing sync command"
     local cmd=(rsync --info=NAME -a)
-    cmd+=(${COMMON_EXCLUDE[@]})
+
+    if [ -v "$EXCLUDE_BASE" ]; then
+        IFS=" " read -r -a exclude <<< "$EXCLUDE_BASE"
+        for e in "${exclude[@]}"; do
+            cmd+=("--exclude \"$e\" ")
+        done
+    fi
 
     if [ "$all" = false ]; then
-        cmd+=(${BUILD_EXCLUDE[@]})
+        log "Exclude EXCLUDE_EXTENDED"
+        if [ -v "$EXCLUDE_EXTENDED" ]; then
+            IFS=" " read -r -a exclude <<< "$EXCLUDE_EXTENDED"
+            for e in "${exclude[@]}"; do
+                cmd+=("--exclude \"$e\" ")
+            done
+        fi
     fi
 
     if [ "$force" = true ]; then
+        log "Force enabled"
         cmd+=(--delete)
     fi
 
     cmd+=( "$SRC" )
     cmd+=( "$DST" )
 
-    echo "${cmd[@]}"
-}
+    log "Complete CMD: \"${cmd[@]}\""
 
-# Returns a list of files to watch
-function get_watch_files() {
-    eval "${WATCH_FILES_CMD[@]}"
+    echo "${cmd[@]}"
 }
 
 sync_cmd=$(get_sync_cmd)
 
 if [ "$daemon" = true ]; then
     # TODO: should be update this list inside loop?
-    watch_files=$(get_watch_files)
-    log "Watching files: \n$watch_files"
+    if [ -z "$WATCH" ]; then
+        echo "Cannot start daemon withoug watching files"
+        exit 6
+    fi
+    log "Watching files: \n$WATCH"
     log "Running upon event: $sync_cmd"
 
     while true; do
@@ -125,22 +199,22 @@ if [ "$daemon" = true ]; then
         retVal=$?
 
         if [ $retVal -eq 124 ]; then
-            log_err "Timeout triggered ($SYNC_TIMEOUT seconds)! Retry..."
+            log_err_msg "Timeout triggered ($SYNC_TIMEOUT seconds)! Retry..."
             continue
         fi
 
         if [ $? -eq 125 ]; then
-            log_err "timeout utility failed, exit"
+            log_err_msg "timeout utility failed, exit"
             exit $?
         fi
 
         if [ $retVal -eq 125 ]; then
-            log_err "timeout utility failed, exit"
+            log_err_msg "timeout utility failed, exit"
             exit $?
         fi
 
         if [ $retVal -ne 0 ]; then
-            log_err "Rsync Error Encountered: $retVal, exit"
+            log_err_msg "Rsync Error Encountered: $retVal, exit"
             exit $?
         fi
 
@@ -149,14 +223,14 @@ if [ "$daemon" = true ]; then
 
         # Wait for event to happen
         # Editors may not modify, but "replace" the file => monitor other events too
-        inotifywait -e modify -e delete_self -e move_self $watch_files
+        inotifywait -e modify -e delete_self -e move_self $WATCH
 
         if [ $? -ne 0 ]; then
-            log_err "inotifywait failed, exit"
+            log_err_msg "inotifywait failed, exit"
             exit $?
         fi
     done
 else
     log "Running: $sync_cmd"
-    eval "$sync_cmd"
+    $sync_cmd
 fi
