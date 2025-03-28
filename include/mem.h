72a0d1184c488687b7273a789037b3c0148adddc
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2025-03-28 19:19:48 +0100

    Add functions to allocate memory at any address

--- include/mem.h
+++ include/mem.h
@@ -10,12 +10,13 @@
 #define UARF_LOG_TAG UARF_LOG_TAG_MEM
 #endif
 
-#define MMAP_FLAGS (MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED_NOREPLACE)
-#define PROT_RW    (PROT_READ | PROT_WRITE)
-#define PROT_RWX   (PROT_RW | PROT_EXEC)
+#define MMAP_FLAGS     (MAP_ANONYMOUS | MAP_PRIVATE)
+#define MMAP_FLAGS_FIX (MMAP_FLAGS | MAP_FIXED_NOREPLACE)
+#define PROT_RW        (PROT_READ | PROT_WRITE)
+#define PROT_RWX       (PROT_RW | PROT_EXEC)
 
 static inline void uarf_map_or_die(void *addr_p, size_t size) {
-    if (mmap(addr_p, size, PROT_RWX, MMAP_FLAGS, -1, 0) == MAP_FAILED) {
+    if (mmap(addr_p, size, PROT_RWX, MMAP_FLAGS_FIX, -1, 0) == MAP_FAILED) {
         UARF_LOG_ERROR("Failed to map %luB at 0x%lx\n", size, _ul(addr_p));
         exit(1);
     };
@@ -23,10 +24,29 @@ static inline void uarf_map_or_die(void *addr_p, size_t size) {
 
 static inline void uarf_map_huge_or_die(void *addr_p, size_t size) {
     // TODO: assert that a huge page is actually mapped
-    if (mmap(addr_p, size, PROT_RWX, MMAP_FLAGS | MAP_HUGETLB, -1, 0) == MAP_FAILED) {
+    if (mmap(addr_p, size, PROT_RWX, MMAP_FLAGS_FIX | MAP_HUGETLB, -1, 0) == MAP_FAILED) {
         UARF_LOG_ERROR("Failed to map %luB at 0x%lx\n", size, _ul(addr_p));
         exit(1);
     };
+}
+
+static inline uint64_t uarf_alloc_map_or_die(size_t size) {
+    void *addr = mmap(NULL, size, PROT_RWX, MMAP_FLAGS, -1, 0);
+    if (addr == MAP_FAILED) {
+        UARF_LOG_ERROR("Failed to map %luB\n", size);
+        exit(1);
+    };
+    return _ul(addr);
+}
+
+static inline uint64_t uarf_alloc_map_huge_or_die(size_t size) {
+    // TODO: assert that a huge page is actually mapped
+    void *addr = mmap(NULL, size, PROT_RWX, MMAP_FLAGS | MAP_HUGETLB, -1, 0);
+    if (addr == MAP_FAILED) {
+        UARF_LOG_ERROR("Failed to map %luB\n", size);
+        exit(1);
+    };
+    return _ul(addr);
 }
 
 static inline void uarf_unmap_or_die(void *addr_p, size_t size) {
--- include/page.h
+++ include/page.h
@@ -6,6 +6,14 @@
 #define PAGE_SHIFT 12
 #endif
 
+#ifndef PAGE_SHIFT_2M
+#define PAGE_SHIFT_2M 21
+#endif
+
 #ifndef PAGE_SIZE
 #define PAGE_SIZE (_U64(1) << PAGE_SHIFT)
+#endif
+
+#ifndef PAGE_SIZE_2M
+#define PAGE_SIZE_2M (_U64(1) << PAGE_SHIFT_2M)
 #endif
