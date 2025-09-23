5cb64f9a3f70dc67f6d3f2f57108eef7179b4802
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2025-09-23 13:02:22 +0200

    Cleanup by removing all data and dump scripts to tools directory
    The scripts themselves need some cleanup at some point

---
+++ tools/get_system_information.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Print various system information, useful when benchmarking different systems or configurations.
+
+set -e
+set -u
+
+echo "\$ hostname"
+hostname
+
+echo "\$ cat /proc/cmdline"
+cat /proc/cmdline
+
+echo "\$ name -a"
+uname -a
+
+echo "\$ cat /etc/lsb-release"
+cat /etc/lsb-release
+
+echo "\$ cat /proc/cpuinfo | grep 'microcode' | head -n 1"
+cat /proc/cpuinfo | grep "microcode" | head -n 1
