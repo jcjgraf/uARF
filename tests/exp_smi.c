b87e014379830c4dec4f4561eb4425f5a7d8c29a
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2025-12-18 17:32:26 +0100

    Update AMD version for new PFC setup
    Now we actually also get a signal for AMD. The issue was that the Kernel
    domain was excluded, which is apparently where the SMIs get counted in?

--- tests/exp_smi.c
+++ tests/exp_smi.c
@@ -9,6 +9,7 @@
 #include "pfc_amd.h"
 #include "spec_lib.h"
 #include "test.h"
+#include "uarch.h"
 
 #include <sys/io.h>
 
@@ -18,85 +19,38 @@
 #endif
 
 /**
- * Trigger and count SMI using kernel module (AMD only)
- *
- * Try to trigger SMI and detect using kernel module. But we get no signal.
- */
-// UARF_TEST_CASE(amd_basic) {
-//     uarf_pi_init();
-//
-//     UarfPfc pfc;
-//     uarf_pfc_init(&pfc, UARF_AMD_LS_SMI_RX);
-//
-//     uarf_pfc_reset(&pfc);
-//     uarf_pfc_start(&pfc);
-//
-//     // for (size_t i = 0; i < 0xffffffff; i++) {
-//     //     // if (!i % 0xff) {
-//     //     uint64_t val = uarf_pfc_read(&pfc);
-//     //     UARF_LOG_INFO("%lu, %lu\n", i, val);
-//     //     // }
-//     //     barf_pi_out(0x0, i);
-//     // }
-//
-//     while (1) {
-//         sleep(10);
-//         uint64_t val = uarf_pfc_read(&pfc);
-//
-//         printf("%lu\n", val);
-//     }
-//
-//     uarf_pfc_stop(&pfc);
-//     uint64_t val = uarf_pfc_read(&pfc);
-//
-//     printf("%lu\n", val);
-//
-//     uarf_pfc_deinit(&pfc);
-//
-//     uarf_pi_deinit();
-//     UARF_TEST_PASS();
-// }
-
-/**
- * Trigger and count SMI using kernel module (Intel only)
+ * Trigger and count SMI using kernel module
  *
  * Run 3 out instructions via the kernel module and verify that they triggered
- * an SMI by reading the SMI count MSR.
- */
-UARF_TEST_CASE(intel_basic) {
-    uarf_pi_init();
-
-    uint64_t start = uarf_rdmsr_user(MSR_SMI_COUNT);
-
-    uarf_out_kmod(0xb2, 0x0);
-    uarf_out_kmod(0xb2, 0x0);
-    uarf_out_kmod(0xb2, 0x0);
-
-    uint64_t val = uarf_rdmsr_user(MSR_SMI_COUNT) - start;
-
-    printf("%lu\n", val);
-    UARF_TEST_ASSERT(val == 3);
-
-    uarf_pi_deinit();
-    UARF_TEST_PASS();
-}
-
-/**
- * Trigger and count SMI in CPL (Intel only)
+ * an SMI by reading the SMI count MSR/PFC.
  *
- * Run 3 `out` instructions in the CPL and verify that they triggered an SMI by
- * reading the SMI count MSR.
+ * Intel counts SMIs using a MSR, AMD using a PFC.
  */
-UARF_TEST_CASE(intel_basic_supervisor) {
+UARF_TEST_CASE(basic) {
     uarf_pi_init();
 
-    uint64_t start = uarf_rdmsr(MSR_SMI_COUNT);
+#if UARF_IS_INTEL()
+    uint64_t start = uarf_rdmsr_user(MSR_SMI_COUNT);
+#elif UARF_IS_AMD()
+    UarfPfc pfc;
+    uarf_pfc_init(&pfc, UARF_AMD_LS_SMI_RX, 0);
+    uarf_pfc_reset(&pfc);
+    uarf_pfc_start(&pfc);
+#endif
 
-    uarf_out(0xb2, 0x0);
-    uarf_out(0xb2, 0x0);
-    uarf_out(0xb2, 0x0);
+    uarf_out_kmod(0xb2, 0x0);
+    uarf_out_kmod(0xb2, 0x0);
+    uarf_out_kmod(0xb2, 0x0);
 
-    uint64_t val = uarf_rdmsr(MSR_SMI_COUNT) - start;
+    uint64_t val;
+
+#if UARF_IS_INTEL()
+    val = uarf_rdmsr_user(MSR_SMI_COUNT) - start;
+#elif UARF_IS_AMD()
+    uarf_pfc_stop(&pfc);
+    val = uarf_pfc_read(&pfc);
+    uarf_pfc_deinit(&pfc);
+#endif
 
     printf("%lu\n", val);
     UARF_TEST_ASSERT(val == 3);
@@ -108,13 +62,14 @@ UARF_TEST_CASE(intel_basic_supervisor) {
 /**
  * Repeatedly trigger SMIs.
  *
- * Can be measured in a different process.
+ * Can be measured in a different process. Does not work for AMD with the PFC,
+ * as PFCs are local to the process.
  */
-UARF_TEST_CASE(intel_trigger) {
+UARF_TEST_CASE(trigger) {
     uarf_pi_init();
 
     while (1) {
-        uarf_pi_out(0x0, 0xb2);
+        uarf_out_kmod(0xb2, 0x0);
         sleep(1);
     }
 
@@ -124,24 +79,44 @@ UARF_TEST_CASE(intel_trigger) {
 /**
  * Iteratively measure SMIs.
  */
-UARF_TEST_CASE(intel_signal) {
+UARF_TEST_CASE(signal) {
     uarf_pi_init();
+
+    // uint64_t start = uarf_rdmsr_user(MSR_SMI_COUNT);
+#if UARF_IS_INTEL()
     uint64_t start = uarf_rdmsr_user(MSR_SMI_COUNT);
+#elif UARF_IS_AMD()
+    UarfPfc pfc;
+    uarf_pfc_init(&pfc, UARF_AMD_LS_SMI_RX, 0);
+    uarf_pfc_reset(&pfc);
+    uarf_pfc_start(&pfc);
+#endif
+
+    uint64_t val;
 
     while (1) {
-        uint64_t val = uarf_rdmsr_user(MSR_SMI_COUNT) - start;
+#if UARF_IS_INTEL()
+        val = uarf_rdmsr_user(MSR_SMI_COUNT) - start;
+#elif UARF_IS_AMD()
+        uarf_pfc_stop(&pfc);
+        val = uarf_pfc_read(&pfc);
+#endif
         printf("%lu\n", val);
         sleep(1);
     }
+
+#if UARF_IS_AMD()
+    uarf_pfc_deinit(&pfc);
+#endif
     uarf_pi_deinit();
     UARF_TEST_PASS();
 }
 
 UARF_TEST_SUITE() {
-    // UARF_TEST_RUN_CASE(amd_basic);
-    UARF_TEST_RUN_CASE(intel_basic);
+
+    UARF_TEST_RUN_CASE(basic);
     // UARF_TEST_RUN_CASE(intel_basic_supervisor);
-    // UARF_TEST_RUN_CASE(intel_trigger);
-    // UARF_TEST_RUN_CASE(intel_signal);
+    // UARF_TEST_RUN_CASE(trigger);
+    // UARF_TEST_RUN_CASE(signal);
     return 0;
 }
