0c839ee15e6b2b11607b581b428f997b3b56f435
Jean-Claude Graf <mail@jeanclaudegraf.ch>
2025-12-19 10:24:29 +0100

    Make code compile without defining a UARCH
    This is useful for code that is uarch agnostic.

--- include/uarch.h
+++ include/uarch.h
@@ -11,21 +11,29 @@
 #define UARF_AMD 0x200
 #define ZEN4     (UARF_AMD + 0x4)
 
+#if UARCH + 0 == 0
+#pragma message "Set default UARCH"
+#define UARCH 0
+#endif
+
 // If the microarchitecture of the current platform is equal to uarch.
 #define UARF_IS_UARCH(uarch) (uarch == UARCH)
 
 #define UARF_IS_INTEL() (UARCH & UARF_INTEL)
 #define UARF_IS_AMD()   (UARCH & UARF_AMD)
 
-#pragma message "arch: " STR(UARCH)
+// #pragma message "arch: " STR(UARCH)
+
 #if UARF_IS_INTEL()
 #pragma message "Compile for Intel"
 #elif UARF_IS_AMD()
 #pragma message "Compile for AMD"
+#elif UARCH == 0
+#pragma message "Compile for no architecture"
 #else
 #error "Unknown vendor: " STR(UARCH)
 #endif
 
-#if !UARF_IS_UARCH(ALDER_LAKE) && !UARF_IS_UARCH(ZEN4)
+#if UARCH != 0 && !UARF_IS_UARCH(ALDER_LAKE) && !UARF_IS_UARCH(ZEN4)
 #error "Unknown system"
 #endif
